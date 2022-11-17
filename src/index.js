b5d279bd2896d3e12e7d0efcdcce37d0e0886549
Adrian <esteban.adrian.gonz@gmail.com>
2022-11-17 00:32:36 -0300

    format document

--- src/index.js
+++ src/index.js
@@ -5,16 +5,16 @@ const sequelize = require('./database/database.js');
 require('./models/indexModels.js')
 
 const Main = async () => {
-    try {
-        // connecting database
-    await sequelize.sync({ force: true });
-      console.log("Connection has been established successfully.");
-        // connecting server   
-      app.listen(app.get("PORT"));
-      console.log(`Server listening on port ${app.get("PORT")}`);
-    } catch (error) {
-      console.error("Unable to connect to the database:", error);
-    }
+  try {
+    // connecting database
+    await sequelize.sync({ force: false });
+    console.log("Connection has been established successfully.");
+    // connecting server   
+    app.listen(app.get("PORT"));
+    console.log(`Server listening on port ${app.get("PORT")}`);
+  } catch (error) {
+    console.error("Unable to connect to the database:", error);
   }
-  
-  Main();
+}
+
+Main();
