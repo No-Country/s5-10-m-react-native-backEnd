61bf972512b58938596e9c060bc860cd499d66de
Adrian <esteban.adrian.gonz@gmail.com>
2022-11-17 00:33:47 -0300

    created register controller

---
+++ src/controllers/Register.Controller.js
@@ -0,0 +1,34 @@
+const { encryptPw } = require('../helpers/bcrypt');
+const User = require('../models/User');
+
+const register = async (req, res) => {
+  try {
+    const { fullName, email, password } = req.body;
+    const existUser = await User.findOne({ where: { email } });
+  
+    if (existUser) {
+      res.json({
+        status: 'failed',
+        message: 'La direccion de correo se encuentra asociada a una cuenta existente'
+      });
+    } else {
+      const user = new User({
+        fullName,
+        email,
+        password: await encryptPw(password)
+      });
+      const newUser = await user.save();
+      res.json({
+        status: 'success',
+        message: 'Usuario creado'
+      });
+    }
+  } catch (error) {
+    res.json({
+      status: 'failer',
+      message: error.message
+    })
+  }
+};
+
+module.exports = register;
