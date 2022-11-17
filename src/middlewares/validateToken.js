4c07d40967a403ddfa95289b3462919fdb0ed94e
Sheice <75450007+Sheice@users.noreply.github.com>
2022-11-17 13:56:07 -0300

    confirm validate token middleware

--- src/middlewares/validateToken.js
+++ src/middlewares/validateToken.js
@@ -1,25 +1,22 @@
-const User = require('../database/database')
+const User = require('../models/User');
 const jwt = require('jsonwebtoken');
 
 const validateToken = async (req, res, next) => {
   const token = req.headers['x-access-token']
-
     try {
         if(!token){
             return res.status(403).json({status: false, message: "token valido"})
         }
         const verifyToken = jwt.verify(token, process.env.SECRETKEY)
 
-        const user = await User.findOne({where: {email: verifyToken.email}})
+        const user = await User.findOne({where: {email: verifyToken}})
         if(!user){
             return res.status(403).json({success: false, msg:'Token inválido'})
         }
 
-        req.userId = verifyToken.id; 
-
         next()
     } catch (error) {
-        res.status(403).json({status: false, message: "Token inválido"})
+        res.status(403).json({status: false, message: error.message})
     }
 }
 module.exports = validateToken;
--- src/routes/Register.js
+++ src/routes/Register.js
@@ -1,7 +1,9 @@
 const { Router } = require('express');
 const register = require('../controllers/Register.Controller');
 const router = Router();
+const validationMiddleware = require('../middlewares/validationMiddleware');
+const registerValidation = require('../helpers/schemasValidation/registerValidation');
 
-router.post('/register', register);
+router.post('/register', validationMiddleware(registerValidation), register);
 
 module.exports = router;
