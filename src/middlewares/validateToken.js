7cfadd31ecdd31dcfd27170eb349b2e3e3801571
Sheice <75450007+Sheice@users.noreply.github.com>
2022-11-17 13:37:29 -0300

    add middleware token

--- src/middlewares/validateToken.js
+++ src/middlewares/validateToken.js
@@ -1,24 +1,25 @@
 const User = require('../database/database')
+const jwt = require('jsonwebtoken');
 
 const validateToken = async (req, res, next) => {
   const token = req.headers['x-access-token']
 
     try {
         if(!token){
-            return res.status(400).json({status: "failde", message: "Se necesita un token valido"})
+            return res.status(403).json({status: false, message: "token valido"})
         }
         const verifyToken = jwt.verify(token, process.env.SECRETKEY)
 
-        const user = await User.findByPk(verifyToken.id)
+        const user = await User.findOne({where: {email: verifyToken.email}})
         if(!user){
-            return res.json({success: "failde", msg:'Token inválido'})
+            return res.status(403).json({success: false, msg:'Token inválido'})
         }
 
         req.userId = verifyToken.id; 
 
         next()
     } catch (error) {
-        res.status(400).json({status: "failed", message: "Token inválido"})
+        res.status(403).json({status: false, message: "Token inválido"})
     }
 }
 module.exports = validateToken;
