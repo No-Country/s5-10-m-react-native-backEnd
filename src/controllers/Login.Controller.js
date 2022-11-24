5fc8647a6396b1cdd6de86a00b7d906f12e1d9c2
Sheice <75450007+Sheice@users.noreply.github.com>
2022-11-24 09:44:28 -0300

    create route and controller login with the validation schema of express-validator

---
+++ src/controllers/Login.Controller.js
@@ -0,0 +1,33 @@
+const { comparePw } = require('../helpers/bcrypt');
+const User = require('../models/User');
+const jwt = require('jsonwebtoken');
+const handleError = require('../helpers/handleError');
+
+const register = async (req, res) => {
+  try {
+    const { fullName, email, password } = req.body;
+    const existUser = await User.findOne({ where: { email } });
+
+    if (existUser) {
+     handleError( res , 400 , "El usuario no existe");
+    } else {
+      const user = new User({
+        fullName,
+        email,
+        password: await encryptPw(password)
+      });
+
+      await user.save();
+
+      res.json({
+        status: true,
+        message: 'Usuario creado',
+        token: jwt.sign(email, process.env.SECRETKEY)
+      });
+    }
+  } catch (error) {
+    handleError( res , 500 , error.message);
+  }
+};
+
+module.exports = register;
