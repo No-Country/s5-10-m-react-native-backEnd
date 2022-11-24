e08ae908525bcb9f87f65114aedf2e9b93fe4925
Sheice <75450007+Sheice@users.noreply.github.com>
2022-11-24 10:41:21 -0300

    add validation to crete skill

--- src/routes/Skill.js
+++ src/routes/Skill.js
@@ -1,9 +1,10 @@
 const { Router } = require('express');
 const router = Router();
 const {createSkill} = require('../controllers/Skill.controller');
+const validationCreateSkill = require('../helpers/schemasValidation/createSkillValidation');
 const validationMiddleware = require('../middlewares/validationMiddleware');
 
 
-router.post('/create/skill', createSkill);
+router.post('/create/skill', validationMiddleware(validationCreateSkill), createSkill);
 
 module.exports = router;
