5ace1c9bedff81133962e7bfe42b600bd23ca218
Adrian <esteban.adrian.gonz@gmail.com>
2022-11-26 14:36:16 -0300

    Route + getCv add to controller

--- src/controllers/Cv.Controller.js
+++ src/controllers/Cv.Controller.js
@@ -6,88 +6,112 @@ const Curriculum = require('../models/Curriculum');
 const handleError = require('../helpers/handleError');
 
 const createCV = async (req, res) => {
-   try {
-    const {userId} = req.params;
-    const {
-        fullName,
-        rol,
-        phone,
-        email,
-        portfolio,
-        linkedin,
-        github,
-        address,
-        aboutMe,
-        experiences,
-        skills,
-        educations
-    } = req.body;
+	try {
+		const { userId } = req.params;
+		const {
+			fullName,
+			rol,
+			phone,
+			email,
+			portfolio,
+			linkedin,
+			github,
+			address,
+			aboutMe,
+			experiences,
+			skills,
+			educations
+		} = req.body;
 
-    const userFound = await User.findByPk(userId);
+		const userFound = await User.findByPk(userId);
 
-    if(!userFound){
-        return handleError(res, 400, "Usuario no encontrado");
-    }
+		if (!userFound) {
+			return handleError(res, 400, "Usuario no encontrado");
+		}
 
-    const curriculumCreated = await Curriculum.create({
-        fullName,
-        rol,
-        phone,
-        email,
-        portfolio,
-        linkedin,
-        github,
-        address,
-        aboutMe,
-        userId
-    });
+		const curriculumCreated = await Curriculum.create({
+			fullName,
+			rol,
+			phone,
+			email,
+			portfolio,
+			linkedin,
+			github,
+			address,
+			aboutMe,
+			userId
+		});
 
-    // CREATE AND JOIN EDUCATIONS TO CV
+		// CREATE AND JOIN EDUCATIONS TO CV
 
-    if(educations){
-        for (const education of educations) {
-            await Education.create({
-                title: education.title,
-                school: education.school,
-                description: education.description,
-                startYear: education.startYear,
-                endYear: education.endYear,
-                cvId: curriculumCreated.id
-            })
-        }
-    }
+		if (educations) {
+			for (const education of educations) {
+				await Education.create({
+					title: education.title,
+					school: education.school,
+					description: education.description,
+					startYear: education.startYear,
+					endYear: education.endYear,
+					cvId: curriculumCreated.id
+				})
+			}
+		}
 
-      // CREATE AND JOIN SKILLS TO CV
+		// CREATE AND JOIN SKILLS TO CV
 
-     if (skills) {
-        for (const skill of skills) {
-            await Skill.create({
-                name: skill.name,
-                cvId: curriculumCreated.id
-            })
-        }
-     }
+		if (skills) {
+			for (const skill of skills) {
+				await Skill.create({
+					name: skill.name,
+					cvId: curriculumCreated.id
+				})
+			}
+		}
 
-    // CREATE AND JOIN EXPERIENCES TO CV
+		// CREATE AND JOIN EXPERIENCES TO CV
 
-    if (experiences) {
-        for (const experience of experiences) {
-            await Experience.create({
-                title: experience.title,
-                description: experience.description,
-                startYear: experience.startYear,
-                endYear: experience.endYear,
-                cvId: curriculumCreated.id
-            })
-        }
-    }
+		if (experiences) {
+			for (const experience of experiences) {
+				await Experience.create({
+					title: experience.title,
+					description: experience.description,
+					startYear: experience.startYear,
+					endYear: experience.endYear,
+					cvId: curriculumCreated.id
+				})
+			}
+		}
 
 
-    res.status(200).json({status: true, message: "CV creado satisfactoriamente!"});
-   } catch (error) {
-    handleError(res, 500, error.message)
-   }
+		res.status(200).json({ status: true, message: "CV creado satisfactoriamente!" });
+	} catch (error) {
+		handleError(res, 500, error.message)
+	}
 
 }
 
-module.exports = {createCV};
+const getCv = async (req, res) => {
+	try {
+		const { userId } = req.body;
+
+		const curriculumFound = await Curriculum.findOne({
+			where: {
+				userId
+			}
+		});
+
+		if (!curriculumFound) {
+			return handleError(res, 400, "Curriculum no encontrado");
+		}
+
+		res.status(200).json({ status: true, message: curriculumFound });
+
+	} catch (error) {
+		handleError(res, 500, error.message)
+	}
+}
+
+module.exports = {
+	createCV,
+	getCv
+};
--- src/routes/cv.js
+++ src/routes/cv.js
@@ -1,10 +1,11 @@
 const { Router } = require('express');
 const validationMiddleware = require('../middlewares/validationMiddleware');
 const createCVValidation = require('../helpers/schemasValidation/createCVValidation');
-const {createCV} = require('../controllers/Cv.Controller');
+const { createCV, getCv } = require('../controllers/Cv.Controller');
 const router = Router();
 
 
 router.post('/user/:userId/create/cv', validationMiddleware(createCVValidation), createCV);
+router.get('/user/:userId/get/cv', getCv);
 
 module.exports = router;
