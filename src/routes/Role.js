5c87e970955c398fc7804745de01626ea99cb81b
Sheice <75450007+Sheice@users.noreply.github.com>
2022-11-28 09:08:23 -0300

    edit role done

--- src/routes/Role.js
+++ src/routes/Role.js
@@ -1,10 +1,11 @@
 const { Router } = require('express');
 const router = Router();
-const {createRole} = require('../controllers/Role.Controller');
+const {createRole, editRole} = require('../controllers/Role.Controller');
 const validationMiddleware = require('../middlewares/validationMiddleware');
 const createRoleValidation = require('../helpers/schemasValidation/createSkillValidation');
 
 
 router.post('/create/role',validationMiddleware(createRoleValidation), createRole);
+router.put('/edit/role/:roleId',editRole);
 
 module.exports = router;
