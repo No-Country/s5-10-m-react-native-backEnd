480b1ddc90676a86ca33bf83e2dc69be8e8461b9
Sheice <75450007+Sheice@users.noreply.github.com>
2022-11-28 09:10:59 -0300

    add validaton for put role

--- src/routes/Role.js
+++ src/routes/Role.js
@@ -2,10 +2,11 @@ const { Router } = require('express');
 const router = Router();
 const {createRole, editRole} = require('../controllers/Role.Controller');
 const validationMiddleware = require('../middlewares/validationMiddleware');
+const validationEditRole = require('../helpers/schemasValidation/editRoleValidation');
 const createRoleValidation = require('../helpers/schemasValidation/createSkillValidation');
 
 
 router.post('/create/role',validationMiddleware(createRoleValidation), createRole);
-router.put('/edit/role/:roleId',editRole);
+router.put('/edit/role/:roleId', validationMiddleware(validationEditRole), editRole);
 
 module.exports = router;
