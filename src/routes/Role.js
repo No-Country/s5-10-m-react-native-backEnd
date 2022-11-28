ea3de10627ed81bd81102a13b6446d67bb5db0fd
Sheice <75450007+Sheice@users.noreply.github.com>
2022-11-28 09:02:15 -0300

    create validation for post role

--- src/routes/Role.js
+++ src/routes/Role.js
@@ -2,8 +2,9 @@ const { Router } = require('express');
 const router = Router();
 const {createRole} = require('../controllers/Role.Controller');
 const validationMiddleware = require('../middlewares/validationMiddleware');
+const createRoleValidation = require('../helpers/schemasValidation/createSkillValidation');
 
 
-router.post('/create/role', createRole);
+router.post('/create/role',validationMiddleware(createRoleValidation), createRole);
 
 module.exports = router;
