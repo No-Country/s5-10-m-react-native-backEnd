3c03d57ca3e75d44a62b71945f3551e7810eee5a
Adrian <esteban.adrian.gonz@gmail.com>
2022-11-30 20:17:08 -0300

    Add new routes get role/skill

--- src/routes/Role.js
+++ src/routes/Role.js
@@ -1,12 +1,13 @@
 const { Router } = require('express');
 const router = Router();
-const {createRole, editRole} = require('../controllers/Role.Controller');
+const { createRole, editRole, getRoles } = require('../controllers/Role.Controller');
 const validationMiddleware = require('../middlewares/validationMiddleware');
 const validationEditRole = require('../helpers/schemasValidation/editRoleValidation');
 const createRoleValidation = require('../helpers/schemasValidation/createSkillValidation');
 
 
-router.post('/create/role',validationMiddleware(createRoleValidation), createRole);
+router.get('/get/roles', getRoles);
+router.post('/create/role', validationMiddleware(createRoleValidation), createRole);
 router.put('/edit/role/:roleId', validationMiddleware(validationEditRole), editRole);
 
 module.exports = router;
