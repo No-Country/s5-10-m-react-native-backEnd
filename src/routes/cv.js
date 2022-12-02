dcb4ebe0c0365c424c5d1e7de811e5cf10c819c9
Adrian <esteban.adrian.gonz@gmail.com>
2022-12-02 04:38:59 -0300

    New route for getCV

--- src/routes/cv.js
+++ src/routes/cv.js
@@ -2,14 +2,16 @@ const { Router } = require('express');
 const validationMiddleware = require('../middlewares/validationMiddleware');
 const createCVValidation = require('../helpers/schemasValidation/createCVValidation');
 const getCVValidation = require('../helpers/schemasValidation/getCVValidation');
+const getCVsValidation = require('../helpers/schemasValidation/getCVsValidation');
 const deleteCVValidation = require('../helpers/schemasValidation/deleteCVValidation');
 const  editCVValidation = require('../helpers/schemasValidation/editCvValidation');
-const { createCV, getCV, deleteCV, editCv } = require('../controllers/Cv.Controller');
+const { createCV, getCV, getCVs, deleteCV, editCv } = require('../controllers/Cv.Controller');
 const router = Router();
 
 
 router.post('/user/:userId/create/cv', validationMiddleware(createCVValidation), createCV);
-router.get('/user/:userId/get/cv', validationMiddleware(getCVValidation), getCV);
+router.get('/user/:userId/get/cv', validationMiddleware(getCVsValidation), getCVs);
+router.get('/user/:userId/get/:cvId', validationMiddleware(getCVValidation), getCV);
 router.delete('/user/:userId/delete/:cvId', validationMiddleware(deleteCVValidation), deleteCV);
 router.put('/user/:userId/edit/cv/:cvId', validationMiddleware(editCVValidation), editCv );
 
